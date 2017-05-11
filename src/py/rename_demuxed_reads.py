f6c071beea2c29d603b400a4887eea8216adfa95
Tom Harrop <twharrop@gmail.com>
2017-05-12 10:31:09 +1200

    remuxing code complete

--- src/py/rename_demuxed_reads.py
+++ src/py/rename_demuxed_reads.py
@@ -7,20 +7,39 @@ import os
 import re
 import csv
 
+# output:
+outdir = "output/qiime/split_libraries"
+
 # parse expected_barcodes and nzgl names from project files
 sample_names = {}
 expected_barcodes = {}
 with open('data/project_files/expected_barcodes.csv', 'r') as f:
     rows = csv.reader(f)
+    next(rows)
     for row in rows:
         sample_names[row[0]] = row[1]
         expected_barcodes[row[0]] = row[2]
 
+# read primers
+linker_primers = {}
+with open('data/project_files/linker_primers.csv', 'r') as f:
+    rows = csv.reader(f)
+    for row in rows:
+        linker_primers[row[0]] = row[1]
+
+# write sample mapping file
+with open(os.path.join(outdir, "mapping.txt"), 'w') as f:
+    f.write('#SampleID\tBarcodeSequence\tLinkerPrimerSequence\tDescription\n')
+    lines = ['\t'.join([sample_names[x], expected_barcodes[x],
+                        linker_primers[x], sample_names[x]])
+             for x in sample_names]
+    f.writelines('%s\n' % line for line in lines)
+
 # parse sample names
-basename_regex = r'(?P<SN>^.+_S\d+)_.*'
+basename_regex = r'^(?P<SN>2556-\d+)-.*'
 
 # parse barcode
-desc_regex = r'.*\:(?P<BC>.+)$'
+desc_regex = r'^.+\s.+\:(?P<BC>\w+\+\w+)$'
 
 # find merged files
 fastq_files = tompytools.find_all(
@@ -29,42 +48,40 @@ fastq_files = tompytools.find_all(
 # collect modified fastq
 modified_fastq = []
 
-# collect sample name and barcodes for mapping file
-barcode_to_sample = {}
-sample_to_barcode = {}
-
-# TODO:
-# read the actual barcode and desired sample name from andrew's xlsx. calculate
-# the "distance" between the barcode and the theoretical barcode in the loop
-
-# loop over fastq
-for fq in fastq_files:
-    # get the sample name
-    bn = os.path.basename(fq)
-    sample_name = re.sub(basename_regex, r'\g<SN>', bn)
-    # unzip and read fq
-    with gzip.open(fq, 'rt') as handle:
-        fastq = SeqIO.parse(handle, 'fastq-sanger')
-        records = [x for x in fastq]
-        # modify records
-        i=0
-        for record in records:
-            i+=1
-            # get the barcode
-            desc = record.description
-            barcode = re.sub(desc_regex, r'\g<BC>', desc)
-            # collect barcode for mapping
-            barcode_to_sample[barcode] = sample_name
-            sample_to_barcode[sample_name] = barcode
-            # write the new id string
-            new_id_string = (
-                "%s_%i %s orig_bc=%s new_bc=%s bc_diffs=0" %
-                (sample_name, i, record.id, barcode, barcode))
-            record.id = new_id_string
-            record.name = ''
-            record.description = ''
-            # collect result
-            modified_fastq.append(record)
+# open a handle for writing fasta output
+with open(os.path.join(outdir, 'seqs.fna'), 'w') as outfile:
+    # loop over fastq
+    for fq in fastq_files:
+        # get the sample name
+        bn = os.path.basename(fq)
+        nzgl_name = re.sub(basename_regex, r'\g<SN>', bn)
+        orig_bc = expected_barcodes[nzgl_name]
+        sample_name = sample_names[nzgl_name]
+        # unzip and read fq
+        with gzip.open(fq, 'rt') as handle:
+            fastq = SeqIO.parse(handle, 'fastq-sanger')
+            records = [x for x in fastq]
+            # modify records
+            i=0
+            for record in records:
+                i+=1
+                # get the barcode
+                desc = record.description
+                new_bc = re.sub(desc_regex, r'\g<BC>', desc)
+                # number of differences between read_bc and exp_bc
+                bc_diff = sum(c1 != c2
+                              for c1, c2
+                              in zip(orig_bc, new_bc))
+                # write the new id string
+                new_id_string = (
+                    "%s_%i %s orig_bc=%s new_bc=%s bc_diffs=%i" %
+                    (sample_name, i, record.id, orig_bc, new_bc, bc_diff))
+                record.id = new_id_string
+                record.name = ''
+                record.description = ''
+                # write result
+                if bc_diff == 0:
+                    SeqIO.write(record, outfile, 'fasta')
 
 # write output to fasta
 SeqIO.write(modified_fastq,
