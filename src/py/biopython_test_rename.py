38531564921272f77a89c5766e839bbf79b4aea2
Tom Harrop <twharrop@gmail.com>
2017-05-11 13:46:07 +1200

    run merge

--- src/py/biopython_test_rename.py
+++ src/py/biopython_test_rename.py
@@ -4,9 +4,13 @@ from Bio import SeqIO
 
 records = [x for x in SeqIO.parse('previous_data/seq45R1.fasta',
                                   'fasta')]
+
+i = 0
 for record in records:
-    new_id_string = ("Test %s orig_bc=TGACCATTTCAC "
-                     "new_bc=TGACCATTTCAC bc_diffs=0" % record.id)
+    i += 1
+    new_id_string = ("Test_%i %s orig_bc=TGACCATTTCAC "
+                     "new_bc=TGACCATTTCAC bc_diffs=0"
+                     % (i, record.id))
     record.id = new_id_string
     record.name = ''
     record.description = ''
