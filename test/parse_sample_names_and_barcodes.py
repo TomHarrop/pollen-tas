12b6c881ce3e5954d7a5414334bf5cfcf5d2c0e0
Tom Harrop <twharrop@gmail.com>
2017-05-12 09:55:31 +1200

    merge code from test *py files into src

---
+++ test/parse_sample_names_and_barcodes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import csv
+from Bio.Seq import Seq
+
+# parse barcode sequences
+bc_seqs = {}
+rc_bc_seqs = {}
+
+with open('data/bc_name_and_seq.csv', 'r') as f:
+    rows = csv.reader(f)
+    next(rows)
+    for row in rows:
+        bc_seqs[row[0]] = Seq(row[1])
+
+for key in bc_seqs:
+    rc_bc_seqs[key] = bc_seqs[key].reverse_complement()
+
+# parse sample name file
+sample_names = {}
+first_bc_rc = {}
+second_bc = {}
+with open('data/sample_id_and_bc.csv', 'r') as f:
+    rows = csv.reader(f)
+    next(rows)
+    for row in rows:
+        sample_names[row[0]] = row[1]
+        first_bc_rc[row[0]] = row[2]
+        second_bc[row[0]] = row[3]
+
+# construct expected barcode
+expected_barcode_seq = {}
+for key in sample_names:
+    expected_barcode_seq[key] = (
+        "%s+%s" %
+        (rc_bc_seqs[first_bc_rc[key]], bc_seqs[second_bc[key]]))
+
+# write output
+csvrows = [[x, sample_names[x], expected_barcode_seq[x]]
+           for x in expected_barcode_seq]
+with open('data/expected_barcodes.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["nzgl_id", "sample_name", "expected_barcodes"])
+    csvwriter.writerows(csvrows)
+
+
